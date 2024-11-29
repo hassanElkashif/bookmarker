ff32cf90054e046f7e2b70c4137526466c552f28
hassanElkashif <hassanelkashif3@gmail.com>
2024-11-29 17:48:43 +0200

    new edits

--- js/main.js
+++ js/main.js
@@ -2,50 +2,51 @@ var websiteNameInput = document.getElementById("siteName");
 var websiteUrlInput = document.getElementById("siteUrl");
 var bookmarks = [];
 
-if (localStorage.getItem('bookmarks') !== null ) {
-
-  bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+if (localStorage.getItem("bookmarks") !== null) {
+  bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
   displayBookmarks();
 }
 
-// adding the values to the empty array  
+// adding the values to the empty array
 function addBookmark() {
-  
-    var bookmark = {
-        siteName : websiteNameInput.value,
-        siteUrl : websiteUrlInput.value
-    }
-    bookmarks.push(bookmark);
-    localStorage.setItem("bookmarks" , JSON.stringify(bookmarks));
-    clearForm();
-    displayBookmarks()
-
+  if (
+    websiteNameInput.classList.contains("is-invalid") ||
+    websiteUrlInput.classList.contains("is-invalid")
+  ) {
+    alertBoxCheck();
+    return;
+  }
+  var bookmark = {
+    siteName: websiteNameInput.value,
+    siteUrl: websiteUrlInput.value,
+  };
+  bookmarks.push(bookmark);
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  clearForm();
+  displayBookmarks();
 }
-
 
 // resetting the form to blanks
 function clearForm() {
-  
   websiteNameInput.value = null;
   websiteUrlInput.value = null;
-  websiteUrlInput.classList.remove('is-valid');
-  websiteNameInput.classList.remove('is-valid');
-  websiteUrlInput.classList.remove('is-invalid');
-  websiteNameInput.classList.remove('is-invalid');
-
+  websiteUrlInput.classList.remove("is-valid");
+  websiteNameInput.classList.remove("is-valid");
+  websiteUrlInput.classList.remove("is-invalid");
+  websiteNameInput.classList.remove("is-invalid");
 }
 
-// displaying the form values 
+// displaying the form values
 function displayBookmarks() {
-
-    var blackBox = '';
-    for( var i=0; i<bookmarks.length; i++ )
-    {
-      blackBox += ` <tr>
-            <td> ${ i+1 } </td>
-            <td> ${ bookmarks[i].siteName } </td>
+  var blackBox = "";
+  for (var i = 0; i < bookmarks.length; i++) {
+    blackBox += ` <tr>
+            <td> ${i + 1} </td>
+            <td> ${bookmarks[i].siteName} </td>
             <td>
-              <button onclick="urlBookmark('${bookmarks[i].siteUrl}')" class="btn bg-success text-white">
+              <button onclick="urlBookmark('${
+                bookmarks[i].siteUrl
+              }')" class="btn bg-success text-white">
                 <i class="fa-solid fa-eye pe-1"></i>Visit
               </button>
             </td>
@@ -56,59 +57,71 @@ function displayBookmarks() {
               </button>
             </td>
           </tr> `;
-    }
+  }
 
-    document.getElementById("bookmarksTable").innerHTML = blackBox;
+  document.getElementById("bookmarksTable").innerHTML = blackBox;
 }
 
 // remove an index from the array and update the array and local storage
 function removeBookmark(removedIndex) {
-  
-    bookmarks.splice(removedIndex,1);
-    displayBookmarks();
-    localStorage.setItem("bookmarks" , JSON.stringify(bookmarks));
-
+  bookmarks.splice(removedIndex, 1);
+  displayBookmarks();
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
 }
 
 // open the url
 function urlBookmark(url) {
-
   if (!/^https?:\/\//.test(url)) {
-    url = 'https://' + url;  
+    url = "https://" + url;
   }
-  window.open( url , "_blank");
+  window.open(url, "_blank");
 }
 
-
-
-// REGEX 
+// REGEX
 function validateInputName(input) {
-  
-  var regex = /^[a-zA-Z]{3,}$/;
+  var regex = /^[a-zA-Z0-9]{3,}$/;
   var value = input.value.trim();
 
   if (regex.test(value)) {
-    input.classList.remove('is-invalid');
-    input.classList.add('is-valid');
+    input.classList.remove("is-invalid");
+    input.classList.add("is-valid");
   } else {
-    input.classList.remove('is-valid');
-    input.classList.add('is-invalid');
+    input.classList.remove("is-valid");
+    input.classList.add("is-invalid");
   }
-  
 }
 
-
 function validateInputUrl(input) {
-
   var regex = /^[a-zA-Z0-9]+(\.[a-zA-Z]{2,})+$/;
   var value = input.value.trim();
 
   if (regex.test(value)) {
-    input.classList.remove('is-invalid');
-    input.classList.add('is-valid');
+    input.classList.remove("is-invalid");
+    input.classList.add("is-valid");
   } else {
-    input.classList.remove('is-valid');
-    input.classList.add('is-invalid');
+    input.classList.remove("is-valid");
+    input.classList.add("is-invalid");
   }
-
 }
+
+//alert box
+var alert = document.getElementById("alertBox");
+var close = document.getElementById("close");
+
+function alertBoxCheck() {
+  if (
+    websiteUrlInput.classList.contains("is-invalid") ||
+    websiteNameInput.classList.contains("is-invalid")
+  ) {
+    alert.classList.remove("d-none");
+    alert.classList.add("d-block");
+  } else {
+    alert.classList.remove("d-block");
+    alert.classList.add("d-none");
+  }
+}
+
+close.addEventListener("click", () => {
+  alert.classList.remove("d-block");
+  alert.classList.add("d-none");
+});
